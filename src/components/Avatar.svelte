c8a49bd6a8d2cb85bee67deaaf1e06c4121d1c92
httpolar <dev@firefly.party>
2024-06-03 01:32:40 +0400

    feat: avatar components

---
+++ src/components/Avatar.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let username: string;
+	export let src: string | undefined = undefined;
+</script>
+
+<div class="flex h-10 w-10 items-center justify-center rounded-full border border-border">
+	{#if src != null}
+		<img {src} decoding="async" loading="lazy" alt="{username}'s avatar" />
+	{:else}
+		<span class="text-sm font-semibold select-none">{username.at(0)}</span>
+	{/if}
+</div>
