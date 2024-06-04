32513651512a55258ce4f90de152d90d7260ae21
httpolar <dev@firefly.party>
2024-06-05 01:51:34 +0400

    fix: dashboard not properly checking current user & unauthorized component utilized

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import Unauthorized from "@/components/errors/Unauthorized.svelte";
 	import { currentUser } from "@/stores/current-user";
 </script>
 
-{#if currentUser != null}
+{#if $currentUser != null}
 	<slot></slot>
+{:else}
+	<Unauthorized />
 {/if}
