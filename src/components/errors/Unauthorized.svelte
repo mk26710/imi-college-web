40e3f531cf99340b760ba4de5e75e39dca39a02d
httpolar <dev@firefly.party>
2024-06-05 01:50:48 +0400

    feat: unauthorized page as a component

---
+++ src/components/errors/Unauthorized.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Main from "../Main.svelte";
+</script>
+
+<Main class="flex flex-1 items-center justify-center bg-background">
+	<div class="container flex flex-col items-center justify-center">
+		<h2 class="text-8xl font-black">401</h2>
+		<p class="text-muted-foreground">Доступ запрещён, необходимо авторизоваться</p>
+	</div>
+</Main>
