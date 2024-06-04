a7950f618d53a3a6874cb73cafff7db0ab125e34
httpolar <dev@firefly.party>
2024-06-05 00:18:26 +0400

    feat: compare function for regions

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,15 @@
+import type { DictRegion } from "./api-types";
+
 export const getPreferredScheme = () => {
-  return window?.matchMedia?.("(prefers-color-scheme:dark)")?.matches ? "dark" : "light";
+	return window?.matchMedia?.("(prefers-color-scheme:dark)")?.matches ? "dark" : "light";
+};
+
+export const compareRegions = (a: DictRegion, b: DictRegion): number => {
+	let diff = b.sortPriority - a.sortPriority;
+
+	if (diff === 0) {
+		diff = (a.displayValue || a.value).localeCompare(b.displayValue || b.value);
+	}
+
+	return diff;
 };
