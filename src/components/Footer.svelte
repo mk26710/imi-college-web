0523d44b6e0f68de966808d19f95e888da35b752
httpolar <dev@firefly.party>
2024-06-05 00:16:32 +0400

    feat: footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<footer class="border-t border-border bg-background py-4 text-muted-foreground text-sm">
+	<div class="container grid grid-cols-2 grid-flow-row-dense auto-rows-min">
+    <a href="https://cdee.ru/"  class="text-sm hover:underline underline-offset-4">{new Date().getFullYear()} &#169; АНО ПОО Колледж &laquo;МИР&raquo;</a>
+  </div>
+</footer>
