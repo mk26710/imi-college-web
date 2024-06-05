a2638e848b1e9103684d6fe2124020684c879410
httpolar <dev@firefly.party>
2024-06-06 00:46:41 +0400

    feat: new home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
-	import { currentUser } from "@/stores/current-user";
+	import Main from "@/components/Main.svelte";
 </script>
 
 <svelte:head>
 	<title>Приём заявлений</title>
 </svelte:head>
 
-<p>{JSON.stringify($currentUser, null, 2)}</p>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Main class="relative bg-background">
+	<div class="container flex flex-1 flex-col items-center justify-center">
+		<div class="relative">
+			<h2
+				class="bg-gradient-to-b from-sky-700 from-25% via-rose-700 to-amber-300 to-95% bg-clip-text text-[15rem] font-extrabold text-transparent"
+			>
+				МИР
+			</h2>
+		</div>
+	</div>
+</Main>
