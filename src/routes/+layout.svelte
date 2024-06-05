5a97c469a1688dccff717464d374fbbf4592bc3d
httpolar <dev@firefly.party>
2024-06-06 00:47:05 +0400

    refactor: context magic is redundant as $page.data exists

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,11 @@
 <script lang="ts">
 	import type { LayoutData } from "./$types";
-	import { setContext } from "svelte";
-	import { readonly, writable } from "svelte/store";
 	import Header from "@/components/Header.svelte";
 	import Footer from "@/components/Footer.svelte";
 	import "@/css/inter.css";
 	import "@/css/main.css";
 
 	export let data: LayoutData;
-
-	const currentUser = writable(data.currentUser);
-	$: {
-		currentUser.set(data.currentUser);
-	}
-
-	setContext("currentUser", readonly(currentUser));
 </script>
 
 <Header currentUser={data.currentUser} />
