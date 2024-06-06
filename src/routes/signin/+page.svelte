4fa2d64247815df10a3dfbe45b5fa38b8da0b5f1
httpolar <dev@firefly.party>
2024-06-06 19:38:30 +0400

    feat: update login page

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -8,6 +8,11 @@
 	import { login } from "@/lib/api";
 	import { goto, invalidateAll } from "$app/navigation";
 	import type { PageData } from "./$types";
+	import Card from "@/components/ui/card/Card.svelte";
+	import CardContent from "@/components/ui/card/CardContent.svelte";
+	import CardHeader from "@/components/ui/card/CardHeader.svelte";
+	import CardTitle from "@/components/ui/card/CardTitle.svelte";
+	import CardDescription from "@/components/ui/card/CardDescription.svelte";
 
 	export let data: PageData;
 
@@ -39,17 +44,31 @@
 
 <Main>
 	<div class="container flex h-full flex-1 items-center justify-center">
-		<form on:submit|preventDefault={submit} class="flex w-full max-w-96 flex-col gap-4">
-			<div class="flex flex-col gap-1.5">
-				<Label required>Логин</Label>
-				<Input bind:value={username} placeholder="ivan01" required />
-			</div>
-			<div class="flex flex-col gap-1.5">
-				<Label required>Пароль</Label>
-				<Input bind:value={password} placeholder="******" required />
-			</div>
+		<Card class="w-full max-w-96">
+			<CardHeader>
+				<CardTitle>Вход в систему</CardTitle>
+				<CardDescription>Укажите логин и пароль</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<form on:submit|preventDefault={submit} class="flex flex-col gap-4">
+					<div class="flex flex-col gap-1.5">
+						<Label required>Логин</Label>
+						<Input bind:value={username} placeholder="ivan01" autocomplete="username" required />
+					</div>
+					<div class="flex flex-col gap-1.5">
+						<Label required>Пароль</Label>
+						<Input
+							bind:value={password}
+							type="password"
+							autocomplete="current-password"
+							placeholder="******"
+							required
+						/>
+					</div>
 
-			<Button type="submit">Войти</Button>
-		</form>
+					<Button type="submit">Войти</Button>
+				</form>
+			</CardContent>
+		</Card>
 	</div>
 </Main>
