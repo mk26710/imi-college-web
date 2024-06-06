71b2b3d126eebd9a040d2413fb907589df782d67
httpolar <dev@firefly.party>
2024-06-06 19:52:29 +0400

    feat: select component has a background color

--- src/components/ui/Select.svelte
+++ src/components/ui/Select.svelte
@@ -13,7 +13,7 @@
 <select
 	{...$$props}
 	class={cn(
-		"flex h-9 w-[180px] items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
+		"flex h-9 w-[180px] items-center justify-between whitespace-nowrap rounded-md border border-input bg-background px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
 		$$props.class,
 	)}
 	{required}
