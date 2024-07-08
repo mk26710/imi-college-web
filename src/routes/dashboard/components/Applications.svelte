86a9d8c40dae65074e05b7b9fc5ea6b42eae3297
httpolar <dev@firefly.party>
2024-07-08 21:50:13 +0400

    feat: dashboard home application list with delete option

--- src/routes/dashboard/components/Applications.svelte
+++ src/routes/dashboard/components/Applications.svelte
@@ -8,16 +8,12 @@
 		getDictionaryAppStatuses,
 		getDictionaryEduLevels,
 	} from "@/lib/api";
+	import Application from "./Application.svelte";
 
 	export let applications: Awaited<ReturnType<typeof getApplications>>;
 	export let majors: Awaited<ReturnType<typeof getCollegeMajors>>;
 	export let appStatuses: Awaited<ReturnType<typeof getDictionaryAppStatuses>>;
 	export let eduLevels: Awaited<ReturnType<typeof getDictionaryEduLevels>>;
-
-	const dateFormatter = (d: Date): string => {
-		const opt = { day: "2-digit", month: "2-digit", year: "numeric" } as const;
-		return Intl.DateTimeFormat("ru", opt).format(d);
-	};
 
 	const majorById = (id: string) => {
 		return majors?.find((m) => m.id === id);
@@ -32,7 +28,10 @@
 	};
 </script>
 
-<div class="rounded-lg border border-border bg-background px-4 py-6 md:px-8 md:pb-8" id="applications">
+<div
+	class="rounded-lg border border-border bg-background px-4 py-6 md:px-8 md:pb-8"
+	id="applications"
+>
 	<div class="flex flex-row items-start px-4 sm:px-0">
 		<div class="flex-1">
 			<h3 class="text-base font-semibold leading-7 text-primary">Мои заявления</h3>
@@ -55,34 +54,11 @@
 				{@const status = statusById(appl.statusId)}
 				{@const eduLevel = eduLevelById(appl.eduLevelId)}
 
-				<div
-					class="flex flex-col rounded-md border border-border bg-secondary px-2 py-3 text-secondary-foreground md:px-4 md:py-4"
-				>
-					<div class="flex items-center gap-2">
-						<div>
-							<span class="hidden md:inline">Заявление на </span>
-							<span class="font-semibold">{major?.prefix}</span>
-							<span> от </span>
-							<span class="font-semibold">{dateFormatter(new Date(appl.createdAt))}</span>
-						</div>
-						<AppStatusBadge statusValue={status?.displayValue ?? status?.value ?? "Неизвестно"} />
-					</div>
-
-					<div class="mt-2 border-t border-border pt-2">
-						<dl class="flex flex-col md:flex-row gap-1">
-							<dt>Направление:</dt>
-							<dd class="font-semibold">{major?.name}</dd>
-						</dl>
-						<dl class="flex flex-col md:flex-row gap-1">
-							<dt>На базе:</dt>
-							<dd class="font-semibold">{eduLevel?.displayValue ?? eduLevel?.value}</dd>
-						</dl>
-						<dl class="flex flex-col md:flex-row gap-1">
-							<dt>Приоритет:</dt>
-							<dd class="font-semibold">{appl.priority}</dd>
-						</dl>
-					</div>
-				</div>
+				{#if major != null && status != null && eduLevel != null}
+					<Application application={appl} {eduLevel} {status} {major} />
+				{:else}
+					<div>Ошибка загрузки данных</div>
+				{/if}
 			{/each}
 		{/if}
 	</div>
