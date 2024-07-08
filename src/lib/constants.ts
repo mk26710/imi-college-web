77ffb1b606f02e899324886130fd5cf6eaad1c82
httpolar <dev@firefly.party>
2024-07-08 21:49:05 +0400

    feat: custom dpeendency name for invalidation of dashboard home data

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,2 @@
 export const ROOT_DEP = "app:root";
+export const DASHBOARD_HOME = "app:dashboard_home";
--- src/routes/dashboard/+page.ts
+++ src/routes/dashboard/+page.ts
@@ -7,14 +7,17 @@ import {
 	getMyAddress,
 } from "@/lib/api";
 import type { PageLoad } from "./$types";
+import { DASHBOARD_HOME } from "@/lib/constants";
 
-export const load: PageLoad = async ({ fetch }) => {
+export const load: PageLoad = async ({ fetch, depends }) => {
 	const address = await getMyAddress({ fetcher: fetch });
 	const dictGenders = await getDictionaryGenders({ fetcher: fetch });
 	const applications = await getApplications({ fetcher: fetch, targetId: "@me" });
 	const majors = await getCollegeMajors({ fetcher: fetch });
 	const appStatuses = await getDictionaryAppStatuses({ fetcher: fetch });
 	const eduLevels = await getDictionaryEduLevels({ fetcher: fetch });
+
+	depends(DASHBOARD_HOME);
 
 	return { address, dictGenders, applications, majors, appStatuses, eduLevels };
 };
