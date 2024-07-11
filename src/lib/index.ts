65d1a7652aba3f3ac84a1ee10abbeeefce9d1772
httpolar <dev@firefly.party>
2024-07-11 22:38:42 +0400

    feat: sorting compare function for nationalities dictionary

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,21 @@
 import type { DictRegion } from "./api-types";
+import type { DictNationality } from "./api/dictionaries";
 
 export const getPreferredScheme = () => {
 	return window?.matchMedia?.("(prefers-color-scheme:dark)")?.matches ? "dark" : "light";
 };
 
 export const compareRegions = (a: DictRegion, b: DictRegion): number => {
+	let diff = b.sortPriority - a.sortPriority;
+
+	if (diff === 0) {
+		diff = (a.displayValue || a.value).localeCompare(b.displayValue || b.value);
+	}
+
+	return diff;
+};
+
+export const compateNationalities = (a: DictNationality, b: DictNationality): number => {
 	let diff = b.sortPriority - a.sortPriority;
 
 	if (diff === 0) {
