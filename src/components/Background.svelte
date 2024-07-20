5b7781af4e6512645349b188b7fb43718036b462
httpolar <dev@firefly.party>
2024-07-20 16:58:34 +0400

    feat: background component

---
+++ src/components/Background.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<div
+	class="absolute inset-0 -z-10 h-full w-full overflow-hidden bg-background bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:32px_32px]"
+>
+	<div
+		class="absolute left-0 right-0 top-0 -z-20 m-auto mr-[70vw] mt-[20vh] h-[250px] w-[250px] rounded-full bg-purple-600 opacity-20 blur-[100px] dark:bg-purple-400 dark:opacity-10"
+	></div>
+	<div
+		class="absolute left-0 right-0 top-0 -z-10 m-auto mt-[50vh] h-[350px] w-[350px] rounded-full bg-purple-600 opacity-20 blur-[100px] dark:bg-purple-400 dark:opacity-10"
+	></div>
+	<div
+		class="absolute left-0 right-0 top-0 -z-20 m-auto ml-[60vw] mt-[15vh] h-[400px] w-[400px] rounded-full bg-purple-600 opacity-20 blur-[100px] dark:bg-purple-400 dark:opacity-10"
+	></div>
+</div>
