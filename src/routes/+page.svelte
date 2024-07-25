001e0330a0f0d9f01bdd3f1d47fb3eee69f5fb47
httpolar <dev@firefly.party>
2024-07-25 17:33:08 +0400

    feat: new home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 	<div class="container flex flex-1 flex-col items-center justify-center">
 		<div class="relative">
 			<h2
-				class="bg-gradient-to-br from-purple-400 to-purple-900 bg-clip-text text-[10rem] font-extrabold text-transparent"
+				class="bg-gradient-to-br from-purple-300 to-purple-700 bg-clip-text text-[10rem] font-extrabold text-transparent"
 			>
 				<span class="hidden md:inline">Колледж</span> МИР
 			</h2>
